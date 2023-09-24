836c392d116c7e76e781669160152e1f1bf96df3
YuliiaTsaruk <julia.tsaruck@gmail.com>
2023-09-24 10:51:24 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  if (!timerId) {
+    timerId = setInterval(() => {
+      const randomColor = getRandomHexColor();
+      bodyEl.style.backgroundColor = randomColor;
+    }, 1000);
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }
+});
+
+stopBtn.addEventListener('click', () => {
+  if (timerId) {
+    clearInterval(timerId);
+    timerId = null;
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  }
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
